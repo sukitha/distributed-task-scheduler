e7fd3d432c3b6755f300e9d433fd4e85694bacab
anas <anas@frsapp.com>
2022-09-06 00:13:35 +0400

    refactor transaction

--- src/services/TasksManager.ts
+++ src/services/TasksManager.ts
@@ -18,7 +18,7 @@ class TasksManager {
     let status = 'PENDING';
     score = new Date(when).getTime()
     console.log(`Push task ${id} to process later on ${when} `);
-    await this.redis.AddToSortedSet(JSON.stringify(item), score);
+    await this.redis.addToSortedSet(JSON.stringify(item), score);
     status = 'SCHEDULED';
     return ({ item, score, status });
   }
@@ -28,7 +28,7 @@ class TasksManager {
       try {
         await this.redis.lock.acquire(`LOCK:ZSET:ITEMS`);
         try {
-          const value = await this.redis.GetTopFromSortedSet();
+          const value = await this.redis.getTopFromSortedSet();
           if (value && value.length > 0) {
             const item = JSON.parse(value[0]);
             const when = parseFloat(value[1]);
@@ -36,13 +36,8 @@ class TasksManager {
               const task: Task = item[2];
               console.log(`Scheduled event ${task.event.name} ready to process for stream ${task.stream}`);
 
-              const commands: string[][] = [['zrem', 'ZSET:SCHEDULED:ITEMS', value[0]]];
-              if (task.type === 'publish_event') {
-                commands.push(['xadd', task.stream, '*', task.event.name, JSON.stringify(task.event)]);
-              } else {
-                // do something else if not publish_event
-              }
-              await this.redis.transaction(commands);
+              if (task.type === 'publish_event') await this.redis.deleteFromSetAndPublish(task, value[0]);
+              else await this.redis.deleteItemFromSet(value[0]);
             }
           } else {
             //No items to work process wait 1s before check again
--- src/utils/redis/RedisHandler.ts
+++ src/utils/redis/RedisHandler.ts
@@ -4,6 +4,7 @@ import { config } from '../../config';
 // no types
 import { createLock } from 'ioredis-lock';
 import loggerFactory from '../../utils/logging';
+import { Task } from '../../streams/events/scheduler';
 
 const logger = loggerFactory.getLogger('RedisHandler');
 
@@ -20,17 +21,29 @@ export class RedisHandler {
   ) {
     this.client = getNewRedisClient({ host: config.redisHost, port: config.redisPort });
     this.lock = createLock(this.client);
+
+
+    // lua script to replace lock (atomic)
+
+    // this.client.defineCommand('', {
+    //   lua: `
+    //   if redis.call("GET", KEYS[1]) == ARGV[1] then
+    //     return redis.call("DEL", KEYS[1])
+    //   end
+    //   return 0`,
+    //   numberOfKeys: 1,
+    // })
   }
 
   async publishEvent<T>(streamName: string, event: IEvent<T>) {
     await this.client.xadd(streamName, '*', event.name, JSON.stringify(event));
   }
 
-  async transaction(commands: string[][]) {
+  private async transaction(commands: string[][]) {
     const result: [Error | null, any][] = await new Promise((resolve, reject) => {
       this.client.multi(commands).exec((err, res) => {
         if (err) {
-          // do something in case of failure?
+          // do something in case of failure
           logger.error(`Transaction failed, ${JSON.stringify(err)}`);
           reject(err);
         } else {
@@ -42,7 +55,15 @@ export class RedisHandler {
     return result;
   }
 
-  async AddToSortedSet(item, value) {
+  async deleteFromSetAndPublish(task: Task, queueItem: string) {
+    const commands: string[][] = [
+      ['zrem', 'ZSET:SCHEDULED:ITEMS', queueItem],
+      ['xadd', task.stream, '*', task.event.name, JSON.stringify(task.event)]
+    ];
+    await this.transaction(commands);
+  }
+
+  async addToSortedSet(item, value) {
     return new Promise((resolve, reject) => {
       this.client.zadd(`ZSET:SCHEDULED:ITEMS`, value, item, (err, result) => {
         if (err) {
@@ -54,7 +75,7 @@ export class RedisHandler {
     })
   }
 
-  async GetTopFromSortedSet(): Promise<string[] | undefined> {
+  async getTopFromSortedSet(): Promise<string[] | undefined> {
     return new Promise((resolve, reject) => {
 
       this.client.zrange(`ZSET:SCHEDULED:ITEMS`, 0, 0, "WITHSCORES", (err, result) => {
@@ -67,7 +88,7 @@ export class RedisHandler {
     })
   }
 
-  async DeleteItemFromSet(item) {
+  async deleteItemFromSet(item) {
     return new Promise((resolve, reject) => {
 
       this.client.zrem(`ZSET:SCHEDULED:ITEMS`, item, (err, result) => {
