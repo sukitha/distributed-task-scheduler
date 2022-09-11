034fb4af9c6698d148285d75ac6fa917a524193e
anas <anas@frsapp.com>
2022-09-12 01:50:28 +0400

    keep scheduling to load next day tasks

--- src/repositories/TasksRepository.ts
+++ src/repositories/TasksRepository.ts
@@ -4,6 +4,10 @@ import { AuditableRepository } from 'mongo-unit-of-work';
 import UserContext from '../utils/UserContext';
 import { ITask, TaskStatus } from '../models/ITask';
 import { Task } from '../streams/events/scheduler';
+import { dateConstants, getDayBoundariesMs } from '../utils/date';
+import loggerFactory from '../utils/logging';
+
+const logger = loggerFactory.getLogger('TasksRepository');
 
 export class TasksRepository extends AuditableRepository<ITask> {
   constructor(collection: Collection<ITask>, session?: ClientSession) {
@@ -29,6 +33,24 @@ export class TasksRepository extends AuditableRepository<ITask> {
       },
       { upsert: true }
     );
+    return result;
+  }
+
+  async updateToNextDay(_id: string, baseDateMs: number) {
+    const nextDay = getDayBoundariesMs({ daysOffset: 1, baseDateMs });
+    const when = new Date(nextDay.from);
+    const result = await this.findOneAndUpdate(
+      { _id },
+      {
+        $set: {
+          when,
+          //@ts-ignore
+          'data.event.data.from': nextDay.from,
+          'data.event.data.to': nextDay.to,
+        }
+      },
+      { returnDocument: 'after' }
+    )
     return result;
   }
 
